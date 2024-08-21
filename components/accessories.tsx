ca0134b685346023aa0e6cb484486c8d063993f8
kwaitsing <131563118+kwaitsing@users.noreply.github.com>
2024-08-21 17:45:31 +0800

    add children and extra class, EOD

--- components/accessories.tsx
+++ components/accessories.tsx
@@ -1,42 +1,50 @@
-import type React from "react"
 import type { NavBarItem } from "../type"
 import { useNavigate } from "react-router-dom"
 import 'beercss'
-
-const nav = useNavigate()
+import type { ReactNode } from "react"
 
 export const SnackBar = (opt: {
     id: string
     msg: string
+    children?: ReactNode
+    className: string
 }) => {
     return (
-        <div id={opt.id} className="snackbar">
+        <div id={opt.id} className={`snackbar ${opt.className}`}>
             {opt.msg}
+            {opt.children}
         </div>
     )
 }
 
 export const FatalErr = (opt: {
     msg: string
+    children?: ReactNode
+    className: string
 }) => {
-    <>
-        <div className={`overlay blur ${opt.msg ? "active" : ''}`}></div>
-        <dialog className={`blur border no-border responsive small-round ${opt.msg ? "active" : ''}`}>
-            <h5>Err</h5>
-            <span>请尝试重载该应用</span>
-            <div>{opt.msg}</div>
-            <nav className="right-align no-space">
-            </nav>
-        </dialog>
-    </>
+    return (
+        <>
+            <div className={`overlay blur ${opt.msg ? "active" : ''}`}></div>
+            <dialog className={`center-align blur border no-border responsive small-round ${opt.msg ? "active" : ''} ${opt.className}`}>
+                <h5>Err</h5>
+                <span>Please try reloading this APP</span>
+                <div>{opt.msg}</div>
+                {opt.children}
+            </dialog>
+        </>
+    )
 }
 
 export const NavBar = (opt: {
     primaryIndex: number
     items: NavBarItem[]
+    children?: ReactNode
+    className: string
 }) => {
+    const nav = useNavigate()
     return (
-        <div className="nav">
+        <div className={`nav ${opt.className}`}>
+            {opt.children}
             {
                 opt.items.map((obj, index) => {
                     return (
@@ -58,9 +66,13 @@ export const NavBar = (opt: {
 export const Drawer = (opt: {
     primaryIndex: number
     items: NavBarItem[]
+    children?: ReactNode
+    className: string
 }) => {
+    const nav = useNavigate()
     return (
-        <nav className="drawer">
+        <nav className={`drawer ${opt.className}`}>
+            {opt.children}
             {
                 opt.items.map((obj, index) => {
                     return (
@@ -79,14 +91,18 @@ export const ExpandDrawer = (opt: {
     id: string
     primaryIndex: number
     items: NavBarItem[]
+    children?: ReactNode
+    className: string
 }) => {
+    const nav = useNavigate()
     return (
-        <dialog id={opt.id} onClick={event => event.stopPropagation()} className={`left-align max`}>
+        <dialog id={opt.id} onClick={event => event.stopPropagation()} className={`max ${opt.className}`}>
             <button onClick={() => ui('#mobileNav')} className="circle transparent">
                 <div className="row">
                     <i>close</i>
                 </div>
             </button>
+            {opt.children}
             {
                 opt.items.map((obj, index) => {
                     return (
--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
 export * from './components/accessories'
+export * from './type'
